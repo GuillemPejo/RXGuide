e35b87b3c8d56c4932f82ca6403e5372b94cdea2
guillem <guillempejo@gmail.com>
2021-02-01 22:48:51 +0100

    First example: Observer & Observable

--- app/src/main/java/me/guillem/rxexample/MainActivity.java
+++ app/src/main/java/me/guillem/rxexample/MainActivity.java
@@ -2,13 +2,31 @@ package me.guillem.rxexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btRX00Introduccion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpView();
+    }
+
+    private void setUpView() {
+        btRX00Introduccion = findViewById(R.id.btRX00Introduccion);
+        btRX00Introduccion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RX00IntroductionActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/me/guillem/rxexample/RX00IntroductionActivity.java
@@ -0,0 +1,52 @@
+package me.guillem.rxexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class RX00IntroductionActivity extends AppCompatActivity {
+
+    private String TAG = "RX00";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rx00_introduction);
+
+        Observable<String> numbersObservable =
+                Observable.just("1","2","3","4","5","6","7","8","9","10");
+
+        Observer<String> numbersObserver = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe" + " Thread: " + Thread.currentThread().getName());
+            }
+
+            @Override
+            public void onNext(String numero) {
+                Log.d(TAG, "onNext: Number: "+ numero + " Thread: " + Thread.currentThread().getName());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError" + " Thread: " + Thread.currentThread().getName());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: All data has been emitted" + " Thread: " + Thread.currentThread().getName());
+            }
+        };
+
+        numbersObservable
+                .subscribe(numbersObserver);
+    }
+}
+
+
